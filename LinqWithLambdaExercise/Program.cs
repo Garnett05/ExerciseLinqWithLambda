9d173665d14782c907cddf17792a4d3cf24e0719
Garnett05 <charlleslimamota10@hotmail.com>
2020-03-29 00:26:24 -0300

    Ajustes após correção do NelioAlvesFalando

--- LinqWithLambdaExercise/Program.cs
+++ LinqWithLambdaExercise/Program.cs
@@ -12,15 +12,12 @@ namespace LinqWithLambdaExercise
         static void Main(string[] args)
         {
             Console.Write("Enter full file path: ");
-            string path = Console.ReadLine();
-            FileStream fs = null;
-            StreamReader sr = null;
+            string path = Console.ReadLine();            
             List < Product > list = new List<Product>();
             
             try
             {
-                fs = new FileStream(path, FileMode.Open);
-                sr = new StreamReader(fs);
+                using (StreamReader sr = File.OpenText(path)) { 
                 while (!sr.EndOfStream)
                 {
                     string[] field = sr.ReadLine().Split(';');
@@ -28,10 +25,11 @@ namespace LinqWithLambdaExercise
                     double price = double.Parse(field[1], CultureInfo.InvariantCulture);
                     list.Add(new Product (name, price));                                        
                 }
+                }
 
                 var averagePrice = list.Select(x => x.Price).DefaultIfEmpty(0.0).Average();
                 Console.WriteLine("Average price: " + averagePrice.ToString("F2", CultureInfo.InvariantCulture));
-                var products = list.Where(x => x.Price < averagePrice).Select(x => x.Name);
+                var products = list.Where(x => x.Price < averagePrice).OrderByDescending(x => x.Name).Select(x => x.Name);
                 foreach (string p in products)
                 {
                     Console.WriteLine(p);
@@ -42,12 +40,7 @@ namespace LinqWithLambdaExercise
             {
                 Console.WriteLine("An error accurred");
                 Console.WriteLine(e.Message);
-            }
-            finally
-            {
-                if (sr != null) sr.Close(); //Necessário fechar o file e stream reader pois o C# não fecha sozinho ele
-                if (fs != null) fs.Close();
-            }
+            }            
         }
     }
 }
